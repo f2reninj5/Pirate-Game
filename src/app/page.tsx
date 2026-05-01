57846a2d7f972d3ba6673c03146e773fac849f87
f2reninj5 <64966099+f2reninj5@users.noreply.github.com>
2026-05-01 22:50:52 +0100

    Create page layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,32 +1,22 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-      </main>
-    </div>
+    <>
+      <header className="flex flex-row justify-between items-center">
+        <button type="button">Instructions</button>
+        <button type="button">Feedback</button>
+        <h1 className="font-pirate">Pirate Game</h1>
+        <button type="button">Reset</button>
+      </header>
+      <main></main>
+      <footer className="flex flex-row justify-center items-center">
+        <span>
+          Copyright © 2026 Maks Nowak. Licensed under the{" "}
+          <a href="https://www.apache.org/licenses/LICENSE-2.0">
+            Apache License, Version 2.0
+          </a>
+          .
+        </span>
+      </footer>
+    </>
   );
 }
