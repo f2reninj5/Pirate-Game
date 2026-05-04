e9f114a27cd847ec1ff0bac07a14c3f2a5d6ac8f
f2reninj5 <64966099+f2reninj5@users.noreply.github.com>
2026-05-04 20:26:16 +0100

    Add state to grid

--- src/component/grid.tsx
+++ src/component/grid.tsx
@@ -1,19 +1,43 @@
-export default function Grid() {
-  const letters = ["A", "B", "C", "D", "E", "F", "G"];
+"use client";
 
-  const labels = Array.from({ length: 7 }, (_, i) =>
-    letters.map((x) => `${x}${i + 1}`),
-  ).flat();
+import { useState } from "react";
+import { cn } from "@/lib/cn";
+
+const styles = {
+  used: "bg-gray-400",
+};
+
+const letters = ["A", "B", "C", "D", "E", "F", "G"];
+
+const labels = Array.from({ length: 7 }, (_, i) =>
+  letters.map((x) => `${x}${i + 1}`),
+).flat();
+
+export default function Grid() {
+  const [grid, setGrid] = useState<Array<boolean>>(new Array(49).fill(false));
+
+  function toggleCell(i: number) {
+    setGrid((g) => {
+      g = [...g];
+      g[i] = !g[i];
+      return g;
+    });
+  }
 
   return (
     <div className="select-none grid grid-cols-7 gap-2 bg-dark p-4 m-auto rounded-md w-[80vmin] h-[80vmin]">
-      {labels.map((label, _) => (
-        <div
-          className="block bg-light rounded-sm hover:cursor-pointer hover:scale-105 active:scale-100 active:translate-y-0.5"
-          key={label}
+      {grid.map((used, i) => (
+        <button
+          onClick={() => toggleCell(i)}
+          className={cn(
+            "block bg-light rounded-sm hover:cursor-pointer hover:scale-105 active:scale-100 active:translate-y-0.5",
+            used ? styles.used : "",
+          )}
+          key={labels[i]}
+          type="button"
         >
-          <span className="m-1">{label}</span>
-        </div>
+          <span className="m-1">{labels[i]}</span>
+        </button>
       ))}
     </div>
   );
