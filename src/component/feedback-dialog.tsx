fed69dd66d80752f92a2c42853e0423e35cd4c16
f2reninj5 <64966099+f2reninj5@users.noreply.github.com>
2026-05-10 20:52:17 +0100

    Update feedback dialog

--- src/component/feedback-dialog.tsx
+++ src/component/feedback-dialog.tsx
@@ -2,31 +2,21 @@
 
 import { Copy, MessageSquare, Printer } from "lucide-react";
 import Dialog from "@/component/ui/dialog";
+import IconButton from "@/component/ui/icon-button";
 
 export default function FeedbackDialog() {
   return (
-    <Dialog
-      trigger={
-        <button type="button">
-          <MessageSquare />
-        </button>
-      }
-      title="Feedback"
-    >
+    <Dialog trigger={<IconButton icon={MessageSquare} />} title="Feedback">
       <div className="flex flex-row gap-2">
         <span>pirategame@maksnowak.me</span>
-        <button
-          type="button"
+        <IconButton
+          icon={Copy}
           onClick={() => {
             navigator.clipboard.writeText("pirategame@maksnowak.me");
           }}
-        >
-          <Copy />
-        </button>
+        />
         <a href="mailto:pirategame@maksnowak.me?subject=Feedback">
-          <button type="button">
-            <Printer />
-          </button>
+          <IconButton icon={Printer} type="button" />
         </a>
       </div>
     </Dialog>
