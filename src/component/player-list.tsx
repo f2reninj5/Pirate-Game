2fbe1d045eb4276063d791b1b39900049533008f
f2reninj5 <64966099+f2reninj5@users.noreply.github.com>
2026-05-11 20:38:38 +0100

    Extract player component

--- src/component/player-list.tsx
+++ src/component/player-list.tsx
@@ -1,5 +1,18 @@
+import { Edit } from "lucide-react";
 import { useContext } from "react";
 import { GameContext } from "@/context/game-context";
+
+function Player({ player }: { player: string }) {
+  return (
+    <div className="flex flex-row gap-2 bg-gray-400">
+      <input type="checkbox" />
+      <span>{player}</span>
+      <button type="button">
+        <Edit size="1em"></Edit>
+      </button>
+    </div>
+  );
+}
 
 export default function PlayerList() {
   const gameContext = useContext(GameContext);
@@ -7,12 +20,9 @@ export default function PlayerList() {
   const { gameState } = gameContext;
 
   return (
-    <div className="grid grid-cols-2 gap-2">
+    <div className="">
       {gameState.players.map((player) => (
-        <div key={player} className="flex flex-row gap-2">
-          <input type="checkbox" />
-          <span>{player}</span>
-        </div>
+        <Player player={player} key={player}></Player>
       ))}
     </div>
   );
