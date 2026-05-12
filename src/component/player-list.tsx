cd3379c150a5a1efc0fb7e2d02565a83b4624422
f2reninj5 <64966099+f2reninj5@users.noreply.github.com>
2026-05-12 18:26:05 +0100

    Update player list

--- src/component/player-list.tsx
+++ src/component/player-list.tsx
@@ -1,13 +1,15 @@
-import { Edit } from "lucide-react";
-import { useContext } from "react";
+import { Edit, Plus } from "lucide-react";
+import { useContext, useState } from "react";
 import { GameContext } from "@/context/game-context";
 
 function Player({ player }: { player: string }) {
+  const [editing, setEditing] = useState(false);
+  const [name, setName] = useState(player);
+
   return (
-    <div className="flex flex-row gap-2 bg-gray-400">
-      <input type="checkbox" />
-      <span>{player}</span>
-      <button type="button">
+    <div className="flex flex-row gap-2 px-2 rounded-sm w-30 bg-gray-800">
+      <span className="text-nowrap overflow-hidden">{player}</span>
+      <button className="opacity-20 hover:opacity-100" type="button">
         <Edit size="1em"></Edit>
       </button>
     </div>
@@ -20,10 +22,22 @@ export default function PlayerList() {
   const { gameState } = gameContext;
 
   return (
-    <div className="">
-      {gameState.players.map((player) => (
-        <Player player={player} key={player}></Player>
-      ))}
+    <div className="flex flex-col gap-2">
+      <div>Players</div>
+      <div className="flex flex-row gap-2">
+        <input className="w-30" type="text" placeholder="Name" />
+        <button type="button">
+          <Plus size="1em"></Plus>
+        </button>
+      </div>
+      <div className="flex flex-col gap-1 h-[min(300px,50vh)] overflow-scroll pr-3">
+        {gameState.players.map((player) => (
+          <div className="flex flex-row gap-2" key={player}>
+            <input type="checkbox" />
+            <Player player={player} key={player}></Player>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
