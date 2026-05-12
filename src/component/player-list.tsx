19e4c3258f7ec0145c5b59a57127641faaafa177
f2reninj5 <64966099+f2reninj5@users.noreply.github.com>
2026-05-12 19:36:15 +0100

    Remove input outline

--- src/component/player-list.tsx
+++ src/component/player-list.tsx
@@ -12,7 +12,7 @@ export default function PlayerList() {
     <div className="bg-dark shadow-inner shadow-black p-2 rounded-md flex flex-col gap-2">
       <div>Players</div>
       <div className="flex flex-row gap-2">
-        <input className="w-30" type="text" placeholder="Name" />
+        <input className="w-30 outline-none" type="text" placeholder="Name" />
         <button type="button">
           <Plus size="1em"></Plus>
         </button>
