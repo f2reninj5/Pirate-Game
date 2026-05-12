5227147d78699bc5b609bac5e2f35c716c61a2c3
f2reninj5 <64966099+f2reninj5@users.noreply.github.com>
2026-05-12 19:17:53 +0100

    Add editing state to player component

--- src/component/player.tsx
+++ src/component/player.tsx
@@ -1,16 +1,46 @@
-import { Edit } from "lucide-react";
+import { Check, Edit, X } from "lucide-react";
 import { useState } from "react";
 
 export default function Player({ player }: { player: string }) {
   const [editing, setEditing] = useState(false);
-  const [name, setName] = useState(player);
+  const [input, setInput] = useState(player);
 
   return (
-    <div className="flex flex-row gap-2 px-2 rounded-sm w-30 bg-gray-800">
-      <span className="text-nowrap overflow-hidden">{player}</span>
-      <button className="opacity-20 hover:opacity-100" type="button">
-        <Edit size="1em"></Edit>
-      </button>
+    <div className="group flex flex-row gap-2 px-2 rounded-sm w-30 bg-gray-800 overflow-hidden">
+      {editing ? (
+        <>
+          <input
+            autoFocus
+            className="w-15 outline-none"
+            type="text"
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+          />{" "}
+          <button type="button" onClick={() => {}}>
+            <Check size="1em" />
+          </button>
+          <button
+            type="button"
+            onClick={() => {
+              setEditing(false);
+              setInput(player);
+            }}
+          >
+            <X size="1em" />
+          </button>
+        </>
+      ) : (
+        <>
+          <span className="text-nowrap overflow-hidden">{player}</span>
+          <button
+            onClick={() => setEditing(true)}
+            className="hidden group-hover:block opacity-20 hover:opacity-100"
+            type="button"
+          >
+            <Edit size="1em" />
+          </button>
+        </>
+      )}
     </div>
   );
 }
