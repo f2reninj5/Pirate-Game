f40fc4defaae3ff72558817be80f1e42273b72f4
f2reninj5 <64966099+f2reninj5@users.noreply.github.com>
2026-05-13 19:37:55 +0100

    Add filtering to player list

--- src/component/player-list.tsx
+++ src/component/player-list.tsx
@@ -1,10 +1,12 @@
 import { Plus } from "lucide-react";
-import { useContext } from "react";
+import { useContext, useRef, useState } from "react";
 import { GameContext } from "@/context/game-context";
 import Player from "./player";
 
 export default function PlayerList() {
   const gameContext = useContext(GameContext);
+  const ref = useRef<HTMLInputElement | null>(null);
+  const [input, setInput] = useState("");
   if (!gameContext) return null;
   const { gameState } = gameContext;
 
@@ -12,18 +14,35 @@ export default function PlayerList() {
     <div className="bg-dark shadow-inner shadow-black p-2 rounded-md flex flex-col gap-2">
       <div>Players</div>
       <div className="flex flex-row gap-2">
-        <input className="w-30 outline-none" type="text" placeholder="Name" />
+        <input
+          ref={ref}
+          className="w-30 outline-none"
+          type="text"
+          placeholder="Name"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          onKeyDown={(e) => {
+            if (e.code === "Escape") {
+              setInput("");
+              if (ref.current) ref.current.blur();
+            }
+          }}
+        />
         <button type="button">
           <Plus size="1em"></Plus>
         </button>
       </div>
       <div className="flex flex-col gap-1 h-[min(300px,50vh)] overflow-scroll pr-3">
-        {gameState.players.map((player) => (
-          <div className="flex flex-row gap-2" key={player}>
-            <input type="checkbox" />
-            <Player player={player} key={player} />
-          </div>
-        ))}
+        {gameState.players
+          .filter((player) =>
+            player.toLowerCase().startsWith(input.toLowerCase()),
+          )
+          .map((player) => (
+            <div className="flex flex-row gap-2" key={player}>
+              <input type="checkbox" />
+              <Player player={player} key={player} />
+            </div>
+          ))}
       </div>
     </div>
   );
