3d370642d02148577155fa5a49612d6ea60bd762
f2reninj5 <64966099+f2reninj5@users.noreply.github.com>
2026-05-19 23:42:44 +0100

    Add previous choose queue contents to page

--- src/app/choose-queue/page.tsx
+++ src/app/choose-queue/page.tsx
@@ -1,3 +1,29 @@
+"use client";
+
+import { ArrowRight } from "lucide-react";
+import Player from "@/component/player";
+import PlayerList from "@/component/player-list";
+import { useGameContext } from "@/context/game-context";
+import { SelectionProvider } from "@/context/selection-context";
+
 export default function ChooseQueue() {
-  return <>Hello</>;
+  const { chooseQueueState } = useGameContext();
+
+  return (
+    <div className="flex flex-row gap-2 justify-between text-light">
+      <PlayerList></PlayerList>
+      <div className="grid grid-cols-[max-content_auto] gap-1">
+        <span>
+          Next <ArrowRight className="inline" size="1em"></ArrowRight>
+        </span>
+        <SelectionProvider>
+          <div className="flex flex-col gap-1">
+            {chooseQueueState.queue.map((player) => (
+              <Player player={player} key={player} />
+            ))}
+          </div>
+        </SelectionProvider>
+      </div>
+    </div>
+  );
 }
