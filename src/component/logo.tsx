7e22a1a9b81e3e58d123064fb2bd90379b0d32d5
f2reninj5 <64966099+f2reninj5@users.noreply.github.com>
2026-05-19 23:28:11 +0100

    Make logo link to root page

--- src/component/logo.tsx
+++ src/component/logo.tsx
@@ -1,3 +1,9 @@
+import Link from "next/link";
+
 export default function Logo() {
-  return <h1 className="font-pirate text-5xl my-2 select-none">Pirate Game</h1>;
+  return (
+    <Link href="/" className="font-pirate text-5xl my-2 select-none">
+      Pirate Game
+    </Link>
+  );
 }
