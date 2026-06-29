fcc9ed7480e22d18d63d4f9840617024cc7f01a1
f2reninj5 <64966099+f2reninj5@users.noreply.github.com>
2026-06-29 14:40:22 +0100

    Fix choose queue preview changing size when queue too short

--- src/component/choose-queue-preview.tsx
+++ src/component/choose-queue-preview.tsx
@@ -13,14 +13,32 @@ export default function ChooseQueuePreview() {
     >
       <div>Choose Queue</div>
       <div className="flex flex-col items-center gap-1">
-        <div className="bg-gray-400 text-[1em] w-full">
-          {chooseQueueState.queue[0]}
+        <div
+          className="bg-gray-400 text-[1em] w-full"
+          style={{
+            visibility:
+              chooseQueueState.queue.length < 1 ? "hidden" : "visible",
+          }}
+        >
+          {chooseQueueState.queue[0] ?? "."}
         </div>
-        <div className="bg-gray-300 text-[0.75em] w-[75%]">
-          {chooseQueueState.queue[1]}
+        <div
+          className="bg-gray-300 text-[0.75em] w-[75%]"
+          style={{
+            visibility:
+              chooseQueueState.queue.length < 2 ? "hidden" : "visible",
+          }}
+        >
+          {chooseQueueState.queue[1] ?? "."}
         </div>
-        <div className="bg-gray-200 text-[0.5625em] w-[56.25%]">
-          {chooseQueueState.queue[2]}
+        <div
+          className="bg-gray-200 text-[0.5625em] w-[56.25%]"
+          style={{
+            visibility:
+              chooseQueueState.queue.length < 3 ? "hidden" : "visible",
+          }}
+        >
+          {chooseQueueState.queue[2] ?? "."}
         </div>
       </div>
     </Link>
