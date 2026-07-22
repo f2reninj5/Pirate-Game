a1bd5c92788de3bb12580665de3f22b773c2de38
f2reninj5 <64966099+f2reninj5@users.noreply.github.com>
2026-07-22 20:43:50 +0100

    Add renamePlayer function

--- src/lib/player.ts
+++ src/lib/player.ts
@@ -12,3 +12,7 @@ export function comparePlayersByName(player: Player, otherPlayer: Player) {
 export function createPlayer(name: string) {
   return { _id: crypto.randomUUID(), name };
 }
+
+export function renamePlayer(array: Player[], name: string, newName: string) {
+  return array.map((p) => (p.name === name ? { ...p, name: newName } : p));
+}
