501681305ac3d3a8fcf0a9141643c886b9af9280
f2reninj5 <64966099+f2reninj5@users.noreply.github.com>
2026-07-25 20:07:35 +0100

    Style random cell button

--- src/component/random-cell-button.tsx
+++ src/component/random-cell-button.tsx
@@ -6,5 +6,9 @@ import { useGameContext } from "@/context/game-context";
 export default function RandomCellButton() {
   const { gridActions } = useGameContext();
 
-  return <Button onClick={gridActions.useRandomCell}>Random Cell</Button>;
+  return (
+    <Button className="p-5 text-2xl" onClick={gridActions.useRandomCell}>
+      Random Cell
+    </Button>
+  );
 }
