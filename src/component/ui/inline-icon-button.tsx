d13dbb79c9366f71f609116cc9d84823b2575045
f2reninj5 <64966099+f2reninj5@users.noreply.github.com>
2026-07-26 14:08:13 +0100

    Fix inline icon button alignment

--- src/component/ui/inline-icon-button.tsx
+++ src/component/ui/inline-icon-button.tsx
@@ -11,7 +11,7 @@ export default function InlineIconButton({
   return (
     <Button
       className={cn(
-        "bg-transparent text-inherit rounded-full hover:scale-150 duration-200 ease-out outline-none",
+        "bg-transparent text-inherit rounded-full hover:scale-150 duration-200 ease-out outline-none h-lh",
         className,
       )}
       {...props}
